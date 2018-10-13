ebd61def20add055ea2da977700cb978817caca9
Kadeem Vers <kadeemv@outlook.com>
2018-10-13 16:57:06 +0200

    exe-6-3 aanpassing

--- HW Opdrachten/Exercise_6/exe_6_3.py
+++ HW Opdrachten/Exercise_6/exe_6_3.py
@@ -1,13 +1,15 @@
 invoer = "5-9-7-1-7-8-3-2-4-8-7-9"
 getallen_lijst = invoer.split('-')
 getallen_lijst.sort()
-kleinste_getal = min(getallen_lijst)
-grooste_getal = max(getallen_lijst)
-aantal_getallen = len(getallen_lijst)
-som_getallen = sum(getallen_lijst)
+getallen_lijst_int = list(map(int, getallen_lijst))
+
+kleinste_getal = min(getallen_lijst_int)
+grooste_getal = max(getallen_lijst_int)
+aantal_getallen = len(getallen_lijst_int)
+som_getallen = sum(getallen_lijst_int)
 gemiddelde = som_getallen / aantal_getallen
 
-print('Gesorteerde list van ints: {}' .format(getallen_lijst))
+print('Gesorteerde list van ints: {}' .format(getallen_lijst_int))
 print('Groost getal: {}' .format(grooste_getal))
 print('Kleinste getal: {}' .format(kleinste_getal))
 print('Aantal getallen: {} en Som van de getallen: {}' .format(aantal_getallen, som_getallen))
